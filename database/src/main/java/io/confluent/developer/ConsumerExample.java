bc352a8691f11e2c47840e1dd677c297fdf79a5f
root <root@DESKTOP-UK4N1GT.localdomain>
2025-02-15 15:56:15 -0700

    adding to my branch

--- database/src/main/java/io/confluent/developer/ConsumerExample.java
+++ database/src/main/java/io/confluent/developer/ConsumerExample.java
@@ -21,33 +21,30 @@ public class ConsumerExample {
     public static void main(final String[] args) {
 
         final Properties props = new Properties() {{
-            // User-specific properties that you must set
-            put(BOOTSTRAP_SERVERS_CONFIG, "<BOOTSTRAP SERVERS>");
-            put(SASL_JAAS_CONFIG,         "org.apache.kafka.common.security.plain.PlainLoginModule required username='<CLUSTER API KEY>' password='<CLUSTER API SECRET>';");
+            // Load sensitive values from environment variables
+            put(BOOTSTRAP_SERVERS_CONFIG, System.getenv("KAFKA_BOOTSTRAP_SERVERS"));
+            put(SASL_JAAS_CONFIG, "org.apache.kafka.common.security.plain.PlainLoginModule required username='"
+                    + System.getenv("KAFKA_USERNAME") + "' password='" + System.getenv("KAFKA_PASSWORD") + "';");
 
             // Fixed properties
-            put(KEY_DESERIALIZER_CLASS_CONFIG,   StringDeserializer.class.getCanonicalName());
+            put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
             put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
-            put(GROUP_ID_CONFIG,                 "kafka-java-getting-started");
-            put(AUTO_OFFSET_RESET_CONFIG,        "earliest");
-            put(SECURITY_PROTOCOL_CONFIG,        "SASL_SSL");
-            put(SASL_MECHANISM,                  "PLAIN");
+            put(GROUP_ID_CONFIG, "kafka-java-getting-started");
+            put(AUTO_OFFSET_RESET_CONFIG, "earliest");
+            put(SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+            put(SASL_MECHANISM, "PLAIN");
         }};
 
-        final String topic = "purchases";
+        final String topic = "database";
 
         try (final Consumer<String, String> consumer = new KafkaConsumer<>(props)) {
             consumer.subscribe(Arrays.asList(topic));
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
                 for (ConsumerRecord<String, String> record : records) {
-                    String key = record.key();
-                    String value = record.value();
-                    System.out.println(
-                            String.format("Consumed event from topic %s: key = %-10s value = %s", topic, key, value));
+                    System.out.printf("Consumed event from topic %s: key = %-10s value = %s%n", topic, record.key(), record.value());
                 }
             }
         }
     }
-
 }
